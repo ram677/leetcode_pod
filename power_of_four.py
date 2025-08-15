4944b766c3fc86e46138250ff35e6d2c85fcc60f
ram677 <ramprakashreddyvnr@gmail.com>
2025-08-15 20:20:51 +0530

    15 Aug 2025

---
+++ power_of_four.py
@@ -0,0 +1,10 @@
+#342. Power of Four
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n <= 0 or (n & (n - 1)) != 0:
+            return False
+        return (n & 0x55555555) != 0
+ 
+#Time complexity : O(1)
+#Space complexity : O(1)
