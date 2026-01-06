05ce05babdd1a15abde08befe1d0b6db66094a22
ram677 <ramprakashreddyvnr@gmail.com>
2026-01-06 23:35:01 +0530

    06 Jan 2026

---
+++ Daily_Challenge/maximum_level_sum_of_a_binary_tree.py
@@ -0,0 +1,27 @@
+#1161. Maximum Level Sum of a Binary Tree
+
+class Solution:
+  def maxLevelSum(self, root: TreeNode | None) -> int:
+    ans = 0
+    maxLevelSum = -math.inf
+    q = collections.deque([root])
+
+    level = 1
+    while q:
+      levelSum = 0
+      for _ in range(len(q)):
+        node = q.popleft()
+        levelSum += node.val
+        if node.left:
+          q.append(node.left)
+        if node.right:
+          q.append(node.right)
+      if levelSum > maxLevelSum:
+        maxLevelSum = levelSum
+        ans = level
+      level += 1
+
+    return ans
+
+# Time complexity: O(n), where n is the number of nodes in the binary tree.
+# Space complexity: O(m), where m is the maximum number of nodes at any level in the binary tree.
