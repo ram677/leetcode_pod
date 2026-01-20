2ed59e2fabfc83721df2a105f7f272a7a7207238
ram677 <ramprakashreddyvnr@gmail.com>
2026-01-20 23:03:51 +0530

    20 Jan 2026

---
+++ Daily_Challenge/construct_the_minimum_bitwise_array_I.py
@@ -0,0 +1,20 @@
+#3314. Construct the Minimum Bitwise Array I
+
+class Solution:
+  def minBitwiseArray(self, nums: list[int]) -> list[int]:
+    return [-1 if num == 2 else num - self._getLeadingOneOfLastGroupOfOnes(num)
+            for num in nums]
+
+  def _getLeadingOneOfLastGroupOfOnes(self, num: int) -> int:
+    """
+    Returns the leading one of the last group of 1s in the binary
+    representation of num. For example, if num = 0b10111, the leading one of
+    the last group of 1s is 0b100.
+    """
+    leadingOne = 1
+    while (num & leadingOne) > 0:
+      leadingOne <<= 1
+    return leadingOne >> 1
+  
+# Time complexity: O(n), where n is the length of nums.
+# Space complexity: O(1) excluding the output array.
