2a42d8af86b651d714314ef17b6d5cfe302c787f
ram677 <ramprakashreddyvnr@gmail.com>
2026-02-09 00:03:34 +0530

    08 Feb 2026

---
+++ Daily_Challenge/balanced_binary_tree.py
@@ -0,0 +1,22 @@
+#110. Balanced Binary Tree
+
+class Solution:
+  def isBalanced(self, root: TreeNode | None) -> bool:
+    ans = True
+
+    def maxDepth(root: TreeNode | None) -> int:
+      """Returns the height of root and sets ans to false if root unbalanced."""
+      nonlocal ans
+      if not root or not ans:
+        return 0
+      left = maxDepth(root.left)
+      right = maxDepth(root.right)
+      if abs(left - right) > 1:
+        ans = False
+      return max(left, right) + 1
+
+    maxDepth(root)
+    return ans
+
+# Time Complexity: O(n) - We visit each node once to compute its height and check balance.
+# Space Complexity: O(h) - The space used by the recursion stack is proportional to the height of the tree, which is O(n) in the worst case (skewed tree) and O(log n) in the best case (balanced tree).
