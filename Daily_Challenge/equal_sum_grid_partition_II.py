8f3e6b45b1b7ec69017cbf3b7bb0f026e1b9be3c
ram677 <ramprakashreddyvnr@gmail.com>
2026-03-26 22:46:31 +0530

    26 Mar 2026

---
+++ Daily_Challenge/equal_sum_grid_partition_II.py
@@ -0,0 +1,26 @@
+#3548. Equal Sum Grid Partition II
+
+class Solution:
+  def canPartitionGrid(self, grid: list[list[int]]) -> bool:
+    summ = sum(map(sum, grid))
+
+    def canPartition(grid: list[list[int]]) -> bool:
+      topSum = 0
+      seen = set()
+      for i, row in enumerate(grid):
+        topSum += sum(row)
+        botSum = summ - topSum
+        seen |= set(row)
+        if topSum - botSum in (0, grid[0][0],  grid[0][-1], row[0]):
+          return True
+        if len(grid[0]) > 1 and i > 0 and topSum - botSum in seen:
+          return True
+      return False
+
+    return (canPartition(grid) or
+            canPartition(grid[::-1]) or
+            canPartition(list(zip(*grid))[::-1]) or
+            canPartition(list(zip(*grid))))
+  
+# Time Complexity: O(m*n) where m and n are the dimensions of the grid, since we need to iterate through the grid to calculate sums and check conditions.
+# Space Complexity: O(m*n) in the worst case for the seen set, if all elements in the grid are unique.
